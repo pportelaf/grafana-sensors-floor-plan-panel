ef986d3c85089422d06cd8b46c7b75c62ea67ced
pportelaf <pabloportelafrancos@gmail.com>
2021-08-19 00:12:28 +0200

    Update prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,5 @@
 module.exports = {
-  ...require("./node_modules/@grafana/toolkit/src/config/prettier.plugin.config.json"),
+  "trailingComma": "none",
+  "singleQuote": true,
+  "printWidth": 120
 };
